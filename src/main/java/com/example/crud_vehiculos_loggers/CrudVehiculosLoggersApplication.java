f3ccd3ea5b7e9976fd7aff71cd02cfa71694bd01
Carlos-GH23 <20223tn124@utez.edu.mx>
2025-07-02 18:32:23 -0600

    Initial commit

---
+++ src/main/java/com/example/crud_vehiculos_loggers/CrudVehiculosLoggersApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_vehiculos_loggers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVehiculosLoggersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudVehiculosLoggersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_vehiculos_loggers/CrudVehiculosLoggersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_vehiculos_loggers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVehiculosLoggersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
